af9112c762429e8eb9e7bf3762dd257404b2ce70
Mae Cana <mae.cana@outlook.com>
2020-07-03 14:41:53 +0800

    Added Template for poll index

--- django_apps/mc_poll/views.py
+++ django_apps/mc_poll/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
+from django.template import loader
 from django.http import HttpResponse
+from .models import Question
 
 def index(request):
-    return HttpResponse("Hello, World. You are at polls index.")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    template = loader.get_template('polls/index.html')
+    context = {
+        'latest_question_list': latest_question_list
+    }
+    return HttpResponse(template.render(context, request))
 
 def details(request, question_id):
     return HttpResponse("You are looking at Question %s." % question_id)
