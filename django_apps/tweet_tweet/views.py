55f5b44825286bacdaf52b63eaf71a0f073d3c7e
Mae Cana <mae.cana@outlook.com>
2020-07-07 22:38:13 +0800

    Modified create-tweet function to be handle via ajax

--- django_apps/tweet_tweet/views.py
+++ django_apps/tweet_tweet/views.py
@@ -30,8 +30,12 @@ def tweet_create_view(request, *args, **kwargs):
         obj = form.save(commit=False)
         obj.save()
         
+        if request.is_ajax():
+            return JsonResponse({},  status=201) # 201 == created items
+
         if next_url != None and is_safe_url(next_url, ALLOWED_HOSTS):
             return redirect(next_url)
+
         form = FormTweet()
     return render(request, 'tweet_tweet/components/form.html', context={'form':form})
 
