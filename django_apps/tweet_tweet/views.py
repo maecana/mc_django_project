5f6cbe8f7f9460f767f0d7851c04c31546c27b8d
Mae Cana <mae.cana@outlook.com>
2020-07-07 21:53:10 +0800

    Set comments to base.html and views.py

--- django_apps/tweet_tweet/views.py
+++ django_apps/tweet_tweet/views.py
@@ -1,5 +1,7 @@
+# python imports
 import random
 
+# django imports
 from django.shortcuts import render, get_object_or_404, redirect
 from django.http import JsonResponse
 from django.utils.http import is_safe_url
@@ -9,33 +11,39 @@ from django.conf import settings
 from .forms import FormTweet
 from .models import Tweet
 
-
+# GLOBAL VARIABLES
 ALLOWED_HOSTS = settings.ALLOWED_HOSTS
 
 # index view
+# name = index
+# url: '/'
 def index(request, *args, **kwargs):
     return render(request, 'tweet_tweet/pages/index.html', context={}, status=200)
 
 # tweet create view
+# name = create
+# url: 'tweets/create/'
 def tweet_create_view(request, *args, **kwargs):
     form = FormTweet(request.POST or None)
     next_url = request.POST.get('next') or None
     if form.is_valid():
         obj = form.save(commit=False)
         obj.save()
-        print(ALLOWED_HOSTS)
-        print(is_safe_url(next_url, ALLOWED_HOSTS))
+        
         if next_url != None and is_safe_url(next_url, ALLOWED_HOSTS):
             return redirect(next_url)
         form = FormTweet()
     return render(request, 'tweet_tweet/components/form.html', context={'form':form})
 
 # tweet list view
+# name = list
+# url: 'tweets/'
+# return json data of all tweets
 def tweet_list_view(request, *args, **kwargs):
     """
     REST API
-    Consumed by Javascript
-    return JSON data
+    Get all Tweet objects
+    return json response data
     """
     tweets = Tweet.objects.all()
     tweet_list = [{'id': tweet.id, 'content': tweet.content, 'likes': random.randint(0, 122)} for tweet in tweets]
