e7bbc6cf50e69ed7ff4bc2faeb0ef10e172372c4
Mae C <maecana@users.noreply.github.com>
2020-07-08 23:57:22 +0800

    Added User Model

--- django_apps/tweet_tweet/models.py
+++ django_apps/tweet_tweet/models.py
@@ -2,9 +2,15 @@
 import random
 
 # django imports
+from django.conf import settings
 from django.db import models
 
+# create model from conf settings
+User = settings.AUTH_USER_MODEL
+
 class Tweet(models.Model):
+    # create foreign_key for user
+    user = models.ForeignKey(User)
     content = models.TextField(null=True, blank=True)
     image = models.FileField(upload_to='images/', null=True, blank=True)
 
